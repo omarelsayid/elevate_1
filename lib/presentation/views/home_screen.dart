79ba3448474296dc831209f19f16f876648e1c4e
omarelsayid <132921536+omarelsayid@users.noreply.github.com>
2025-02-18 06:42:49 +0200

    Update home_screen.dart

--- lib/presentation/views/home_screen.dart
+++ lib/presentation/views/home_screen.dart
@@ -86,7 +86,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                     );
                   }
-                  return Text('home');
+                  return Text('HOMEEEE');
                 },
                 bloc: homeCubit,
               ),
